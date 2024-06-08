ebd628ad79b0b37efb965ac67eb915354855cc8c
Miguel Miller <mmartinrm3@gmail.com>
2024-06-07 20:37:18 -0500

    chore: Refactor main-page component to use local variables for characterList, onDelete, onNewCharacter

--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
 import { DbzService } from '../services/dbz.service';
+import { Character } from '../interfaces';
 
 @Component({
   selector: 'app-dbz-main-page',
   templateUrl: 'main-page.component.html',
 })
 export class MainPageComponent {
-  constructor(public dbzService: DbzService) {}
+  constructor(private dbzService: DbzService) {}
+
+  get characters(): Character[] {
+    return [...this.dbzService.characters];
+  }
+
+  onDeleteCharacter(id: string): void {
+    this.dbzService.onDeleteCharacter(id);
+  }
+
+  onNewCharacter(character: Character): void {
+    this.dbzService.onNewCharacter(character);
+  }
 }
